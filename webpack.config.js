310a2a55ddeea766dd7bcd18222a96d7dc8739f9
Jonatas Gusmão <js.gusmao@hotmail.com>
2019-05-28 19:41:28 -0300

    update library target

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,8 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'index.js',
+    library: '[name]',
+    libraryTarget: 'umd',
   },
   module: {
     rules: [
